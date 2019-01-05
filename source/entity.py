bf04af35777f4433b4f84a667938fc6c56d3655c
Kleinman <ted.kleinman@hotmail.com>
2019-01-05 03:24:07 +0100

    Minor refactoring of entity

--- source/entity.py
+++ source/entity.py
@@ -1,19 +1,19 @@
 from collections import OrderedDict, namedtuple
-from itertools import combinations
+from itertools import combinations, chain
 from source.linear_algebra import *
 
 
-class Component:
-    def __repr__(self):
-        title = "\n{}:\n".format(self.__class__.__name__)
-        attribute_template = "\t{:<10} = {}"
+# class Component:
+#     def __repr__(self):
+#         title = "\n{}:\n".format(self.__class__.__name__)
+#         attribute_template = "\t{:<10} = {}"
+#
+#         return title + "\n".join(
+#             [attribute_template.format(name, value) for name, value in self.__dict__.items()]
+#         )
 
-        return title + "\n".join(
-            [attribute_template.format(name, value) for name, value in self.__dict__.items()]
-        )
 
-
-class Transform(Component):
+class Transform:
     def __init__(self, location, rotation, scale):
         self.location = Vector3(*location)
         self.rotation = Vector3(*rotation)
@@ -23,27 +23,27 @@ class Transform(Component):
         return transformation_matrix(*self.location, *self.rotation, *self.scale)
 
 
-class Renderable(Component):
+class Renderable:
     def __init__(self, shader, model, *textures):
         self.shader   = shader
         self.model    = model
         self.textures = textures
 
 
-class PointLight(Component):
+class PointLight:
     def __init__(self, color, attenuation):
         self.color = color
         self.attenuation = attenuation
 
 
-class Physics(Component):
+class Physics:
     def __init__(self, velocity, acceleration, max_speed):
         self.velocity = velocity
         self.acceleration = acceleration
         self.max_speed = max_speed
 
 
-class Collidable(Component):
+class Collidable:
     def __init__(self, hitbox):
         self.hitbox = hitbox
 
@@ -77,6 +77,20 @@ Index: 3
 
 """
 
+class IComponentSetStorage:
+    def create(self, *components):
+        raise NotImplemented("Sorry...")
+
+    def get(self, index, *Components):
+        raise NotImplemented("Sorry...")
+
+    def set(self, index, *components):
+        raise NotImplemented("Sorry...")
+
+    def destroy(self, index):
+        raise NotImplemented("Sorry...")
+
+
 class SwitchArray:
 
     def __init__(self, ConstructorClass):
@@ -87,26 +101,25 @@ class SwitchArray:
     def __iter__(self):
         return (self.data[i] for i in range(self.last_occupied + 1))
 
-    def create(self):
-        if 0 <= self.last_occupied < len(self.data)-2:
-            index = self.last_occupied
-            self.data[index] = None
-            self.last_occupied += 1
-        else:
-            self.data.append(None)
-            index = len(self.data) - 2  # Making sure to always have one free slot at the end.
-            self.last_occupied += 1
+    def create(self, *elements):
+        for element in elements:
+            if 0 <= self.last_occupied < len(self.data)-2:
+                index = self.last_occupied
+                self.data[index] = element
+                self.last_occupied += 1
+            else:
+                self.data.append(None)
+                index = self.last_occupied + 1  # Making sure to always have one free slot at the end.
+                self.data[index] = element
+                self.last_occupied = index
 
         return index
 
     def get(self, index):
         return self.data[index]
 
-    def modify(self, index, *args, **kwargs):
-        self.data[index] = self.ConstructorClass(*args, **kwargs)
-
-    def copy(self, index, component):
-        self.data[index] = component
+    def set(self, index, data):
+        self.data[index] = data
 
     def destroy(self, index):
         if not 0 <= index < self.last_occupied:
@@ -124,39 +137,58 @@ class SwitchArray:
         return last_occupied
 
 
-class ComponentSetStorage:
+
+class SortedGroupComponentSetStorage(IComponentSetStorage):
+    def __init__(self):
+        pass
+
+    def create(self, *components):
+        raise NotImplemented("Sorry...")
+
+    def get(self, index, *Components):
+        raise NotImplemented("Sorry...")
+
+    def set(self, index, *components):
+        raise NotImplemented("Sorry...")
+
+    def destroy(self, index):
+        raise NotImplemented("Sorry...")
+
+
+class GeneralComponentSetStorage(IComponentSetStorage):
 
     def __init__(self, *component_classes):
-        self.mapping = {}           # For destruction
+        self.mapping = {}           # When switching in destroy we need to map the last index to its new index.
         self.component_array = {
-            component_class.__name__ : SwitchArray(component_class) for component_class in component_classes
+            component_class : SwitchArray(component_class) for component_class in component_classes
         }
 
-    def create(self):
+    def create(self, *components):
+        assert len(components) == len(self.component_array), "BAD! All must be initialized"
         previous_index = -1
         index = -1
-        for name, array in self.component_array.items():
-            index = array.create()
+        for component in components:
+            array = self.component_array[component.__class__]
+            index = array.create(component)
             assert index == previous_index or previous_index == -1, "All indices must be the same!"
             previous_index = index
         return index
 
-    def get(self, index, Components=None):
+    def get(self, index, *Components):
         components = []
-        if Components is None:
+        if not Components:
             for name, array in self.component_array.items():
                 components.append(array.get(index))
             return components
         else:
-            raise NotImplemented('Sorry')
+            for Component in Components:
+                components.append(self.component_array[Component])
+            return components
 
-    def modify(self, index, Component, *args, **kwargs):
-        component_array = self.component_array[Component.__name__]
-        component_array.modify(index, *args, **kwargs)
-
-    def copy(self, index, Component, component):
-        component_array = self.component_array[Component.__name__]
-        component_array.copy(index, component)
+    def set(self, index, *components):
+        for component in components:
+            component_array = self.component_array[component.__class__]
+            component_array.modify(index, component)
 
     def destroy(self, index):
         indices = []
@@ -172,7 +204,7 @@ class ComponentSetStorage:
 
 class Entity:
     def __init__(self, component_set, index):
-        self.component_set = component_set
+        self.component_set = component_set  # TODO(ted): Should be bitmask instead.
         self.index = index
 
 
@@ -181,84 +213,93 @@ class World:
     def __init__(self):
         self.registered_components = [Transform, Renderable, PointLight, Physics]
         # TODO(ted): Should be dynamic and using a bitmask.
-        self.component_sets = [
-            frozenset((Transform,)), frozenset((Transform, Renderable)), frozenset((Transform, PointLight)),
-            frozenset((Transform, Physics)), frozenset((Transform, Renderable, PointLight)),
-            frozenset((Transform, Renderable, Physics)), frozenset((Transform, PointLight, Physics)),
-            frozenset((Transform, Renderable, PointLight, Physics))
-        ]
-
         # TODO(ted): Make it possible for user to set ComponentSetStorage.
         # TODO(ted): Don't create an entry for every possible combination. Use an hash table and create one when
         # necessary. Delete entries that are not used.
-        self.data = [ComponentSetStorage(*component_set) for component_set in self.component_sets]
+        self.component_sets = {
+            # frozenset((Transform,)):                                 ComponentSetStorage(Transform),
+            # frozenset((Transform, Renderable)):                      ComponentSetStorage(Transform, Renderable),
+            # frozenset((Transform, PointLight)):                      ComponentSetStorage(Transform, PointLight),
+            # frozenset((Transform, Physics)):                         ComponentSetStorage(Transform, Physics),
+            # frozenset((Transform, Renderable, PointLight)):          ComponentSetStorage(Transform, Renderable, PointLight),
+            # frozenset((Transform, Renderable, Physics)):             ComponentSetStorage(Transform, Renderable, Physics),
+            # frozenset((Transform, PointLight, Physics)):             ComponentSetStorage(Transform, PointLight, Physics),
+            # frozenset((Transform, Renderable, PointLight, Physics)): ComponentSetStorage(Transform, Renderable, PointLight, Physics),
+        }
 
-    def register_component(self, Component, Storage=ComponentSetStorage):
-        self.registered_components.append(Component)
-        self.data.append(Storage(Component))
+    def _get_or_create_set_array(self, entity):
+        if entity.component_set not in self.component_sets:
+            self.component_sets[entity.component_set] = GeneralComponentSetStorage(*entity.component_set)
+        return self.component_sets[entity.component_set]
 
-    def create_entity(self):
-        entity = Entity(frozenset(), 0)
-        return entity
+    def register_component(self, Component, Storage=GeneralComponentSetStorage):
+        pass
 
     def get_component_arrays_of(self, *Components):
         component_set_arrays = []
 
         component_target = frozenset(Components)
-        for index, component_set in enumerate(self.component_sets):
+        for component_set, component_set_array in self.component_sets.items():
             if component_target.intersection(component_set) == component_target:
-                component_set_arrays.append(self.data[index])
+                component_set_arrays.append(component_set_array)
 
 
-        def temp(arrays):
-            for array in arrays:
-                for element in array:
+        def temp(sequences):
+            for sequence in sequences:
+                for element in sequence:
                     yield element
 
         result = []
         for Component in Components:
-            arrays = [component_set_array.component_array[Component.__name__] for component_set_array in component_set_arrays]
+            arrays = [component_set_array.component_array[Component] for component_set_array in component_set_arrays]
             result.append(temp(arrays))
-        return result
+        return zip(*result)
 
-    def add_component(self, entity, Component, *args, **kwargs):
+
+
+    def create_entity(self, *components):
+        entity = Entity(frozenset(), 0)
+        if components:
+            self.add_components(entity, *components)
+        return entity
+
+    def destroy_entity(self, entity):
+        if entity.component_set:
+            set_array = self.component_sets[entity.component_set]
+            set_array.destroy(entity.index)
+
+    def add_components(self, entity, *components):
         # Remove
         previous_components = []
         if entity.component_set:
-            set_array_index = self.component_sets.index(entity.component_set)
-            set_array = self.data[set_array_index]
+            set_array = self.component_sets[entity.component_set]
             previous_components = set_array.get(entity.index)
             set_array.destroy(entity.index)
 
         # Create
-        entity.component_set = entity.component_set.union(frozenset((Component,)))  # Union
-        set_array_index = self.component_sets.index(entity.component_set)
-        set_array = self.data[set_array_index]
-        entity.index = set_array.create()
-        for component in previous_components:
-            set_array.copy(entity.index, component.__class__, component)
+        component_set_to_add = frozenset(type(component) for component in components)
+        entity.component_set = entity.component_set.union(component_set_to_add)  # Union
 
-        set_array.modify(entity.index, Component, *args, **kwargs)
+        set_array = self._get_or_create_set_array(entity)
+        entity.index = set_array.create(*chain(previous_components, components))
 
-    def remove_component(self, entity, Component):
+    def remove_components(self, entity, *Components):
         # Remove
         previous_components = []
         if entity.component_set:
-            set_array_index = self.component_sets.index(entity.component_set)
-            set_array = self.data[set_array_index]
+            set_array = self.component_sets[entity.component_set]
             previous_components = set_array.get(entity.index)
             set_array.destroy(entity.index)
 
         # Filter
-        previous_components = [component for component in previous_components if type(component) != Component]
+        previous_components = [component for component in previous_components if type(component) not in Components]
 
         # Create
-        entity.component_set = entity.component_set.difference(frozenset((Component, )))  # Difference, not union.
-        set_array_index = self.component_sets.index(entity.component_set)
-        set_array = self.data[set_array_index]
-        entity.index = set_array.create()
-        for component in previous_components:
-            set_array.copy(entity.index, component.__class__, component)
+        component_set_to_remove = frozenset(Components)
+        entity.component_set = entity.component_set.difference(component_set_to_remove)   # Difference
+
+        set_array = self._get_or_create_set_array(entity)
+        entity.index = set_array.create(*previous_components)
 
 
 
@@ -268,36 +309,38 @@ def main():
 
     a = world.create_entity()
     b = world.create_entity()
-    c = world.create_entity()
-    d = world.create_entity()
+    c = world.create_entity(
+        Transform(location=(5, 4, 22), rotation=(13, 2, 33), scale=(52, 1, 2)),
+        PointLight(color=(2, 43, 5), attenuation=12)
+    )
 
-    world.add_component(b, Transform, location=(1, 1, 1), rotation=(1, 1, 1), scale=(2, 2, 2))
+    world.add_components(b, Transform(location=(1, 1, 1), rotation=(1, 1, 1), scale=(2, 2, 2)))
     arrays = world.get_component_arrays_of(Transform)
 
-    world.add_component(a, Transform, location=(0, 0, 0), rotation=(0, 0, 0), scale=(1, 1, 1))
+    world.add_components(a, Transform(location=(0, 0, 0), rotation=(0, 0, 0), scale=(1, 1, 1)))
     arrays = world.get_component_arrays_of(Transform)
 
-    world.add_component(a, PointLight, color=(1, 2, 3), attenuation=25)
-    transforms, point_lights = world.get_component_arrays_of(Transform, PointLight)
+    world.add_components(a, PointLight(color=(1, 2, 3), attenuation=25))
+    entities = world.get_component_arrays_of(Transform, PointLight)
 
     print('---- START ----')
-    for x in zip(transforms, point_lights):
+    for x in entities:
         print(x)
     print('----- END -----')
 
-    world.add_component(b, PointLight, color=(4, 5, 6), attenuation=11)
-    transforms, point_lights = world.get_component_arrays_of(Transform, PointLight)
+    world.add_components(b, PointLight(color=(4, 5, 6), attenuation=11))
+    entities = world.get_component_arrays_of(Transform, PointLight)
 
     print('---- START ----')
-    for x in zip(transforms, point_lights):
+    for x in entities:
         print(x)
     print('----- END -----')
 
-    world.remove_component(b, PointLight)
-    transforms, point_lights = world.get_component_arrays_of(Transform, PointLight)
+    world.remove_components(b, PointLight)
+    entities = world.get_component_arrays_of(Transform, PointLight)
 
     print('---- START ----')
-    for x in zip(transforms, point_lights):
+    for x in entities:
         print(x)
     print('----- END -----')
 
